d03d30baba5853d15bc58f655fc52fe356c89c19
Christopher Weatherhead <chrisw@privacyinternational.org>
2019-11-10 13:30:34 +0000

    Removed redundant empty line
    that seemed to cause catastrophic failure

--- mitmproxy_start-ios.sh
+++ mitmproxy_start-ios.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 echo "This is undocumented functionality of this toolkit"
 lxsudo mitmweb --anticomp --anticache --listen-host 0.0.0.0 --no-http2 --showhost --save-stream-file ~/mitmproxy-`date +%s`-ios-flow.cap --ssl-insecure
