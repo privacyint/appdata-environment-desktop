88ef00d74019b4ba518534eb7cd59a87b6ae85c5
Christopher Weatherhead <chrisw@privacyinternational.org>
2020-01-15 14:00:32 +0000

    Initial version of new user script

---
+++ main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+while :
+do
+ask=`zenity --list --title="Please Select a task" --column="0" "Configure Wireless Adapter" "Enable hostapd" "Run an APK through APK-Mitm" "Start mitmproxy" "Show the documentation" --width=200 --height=300 --hide-header`
+if [ "$ask" == "Configure Wireless Adapter" ]; then
+    bash ./systemcfg-scripts/change_wireless_interface.sh
+fi
+
+if [ "$ask" == "Enable hostapd" ]; then
+    bash ./systemcfg-scripts/administer_service.sh
+fi
+
+if [ "$ask" == "Run an APK through APK-Mitm" ]; then
+    bash ./apkman-scripts/zenity-apkmitm.sh
+fi
+
+if [ "$ask" == "Start mitmproxy" ]; then
+    ./mitmproxy-scripts/mitmproxy_start.sh
+fi
+
+if [ "$ask" == "Show the documentation" ]; then
+    grip -b ~/Desktop
+fi
+done
+exit 0
