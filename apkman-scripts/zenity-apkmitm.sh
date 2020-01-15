302ab838108908482f1cbc32cf5912c4859613a1
Christopher Weatherhead <chrisw@privacyinternational.org>
2020-01-15 13:35:33 +0000

    APKmitm setup script

--- apkman-scripts/zenity-apkmitm.sh
+++ apkman-scripts/zenity-apkmitm.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo placeholder
+echo "This Script Bootstraps APK's to work with non-rooted devices"
+zenity --info --no-wrap --text="This program is an assistant for making APKs compatible with mitmproxy\nPlease select a APK File to process"
+APKFILE=$(zenity --file-selection --file-filter="*.apk")
+mkdir ~/apk-mitm-processed
+cd ~/apk-mitm-processed
+sudo apk-mitm $APKFILE
+# zenity --progress --text="Processing APK File:\n$APKFILE" --pulsate
+pcmanfm-qt -n ~/apk-mitm-processed
+echo Complete
 exit 0
