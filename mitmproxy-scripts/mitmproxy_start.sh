498ded6b8bb6caf95f794758afed044af09a1e41
Christopher Weatherhead <chrisw@privacyinternational.org>
2020-01-17 12:17:07 +0000

    Reinstated the cancel button!

--- mitmproxy-scripts/mitmproxy_start.sh
+++ mitmproxy-scripts/mitmproxy_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 lxsudo mitmweb --anticomp --anticache --listen-host 0.0.0.0 --mode transparent --showhost --save-stream-file ~/mitmproxy-`date +%s`-flow.cap --ssl-insecure &
-zenity --progress --text="MITMPRoxy is Running, close this dialog to exit" --pulsate --no-cancel
+zenity --progress --text="MITMPRoxy is Running\nClose this dialog to exit mitmproxy" --pulsate
 lxsudo pkill mitmweb
 exit 0
