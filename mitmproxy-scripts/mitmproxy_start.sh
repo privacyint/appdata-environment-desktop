0a9d0d4f66bfb6770a15f3ba36df3f5a4ae35ce0
Christopher Weatherhead <chrisw@privacyinternational.org>
2020-01-17 12:01:36 +0000

    mitmproxy dialog version 1

--- mitmproxy-scripts/mitmproxy_start.sh
+++ mitmproxy-scripts/mitmproxy_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-lxsudo mitmweb --anticomp --anticache --listen-host 0.0.0.0 --mode transparent --showhost --save-stream-file ~/mitmproxy-`date +%s`-flow.cap --ssl-insecure
+lxsudo mitmweb --anticomp --anticache --listen-host 0.0.0.0 --mode transparent --showhost --save-stream-file ~/mitmproxy-`date +%s`-flow.cap --ssl-insecure &
+zenity --progress --text="MITMPRoxy is Running, close this dialog to exit" --pulsate --no-cancel
+lxsudo pkill mitmweb
 exit 0
