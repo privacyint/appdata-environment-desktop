c314b357ab70b78825d2e23c61045acca224a053
Christopher Weatherhead <chrisw@privacyinternational.org>
2020-07-07 14:44:46 +0100

    Minor UI Changes, new update mode

--- apkman-scripts/zenity-apkmitm.sh
+++ apkman-scripts/zenity-apkmitm.sh
@@ -4,7 +4,8 @@ zenity --info --no-wrap --text="This program is an assistant for making APKs com
 APKFILE=$(zenity --file-selection --file-filter="*.apk")
 mkdir ~/apk-mitm-processed
 cd ~/apk-mitm-processed
-lxsudo apk-mitm $APKFILE
+qterminal --workingfir ~/apk-mitm-processed --execute "lxsudo apk-mitm $APKFILE"
 ## zenity --progress --text="Processing APK File:\n$APKFILE" --pulsate --no-cancel --auto-kill
 pcmanfm-qt -n $(dirname $APKFILE) &
+zenity --info --no-wrap --text="apk-mitm Complete"
 echo Complete
--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-ask=`zenity --list --title="Please Select a task" --column="0" "Configure Wireless Adapter" "Enable hostapd" "Run an APK through APK-Mitm" "Start mitmproxy" "Show the documentation" "Exit" --width=200 --height=300 --hide-header`
+ask=`zenity --list --title="Please Select a task" --column="0" "Configure Wireless Adapter" "Enable hostapd" "Run an APK through APK-Mitm" "Start mitmproxy" "Show the documentation" "Update -- Experimental" "Exit" --width=450 --height=400 --hide-header`
 if [ "$ask" == "Configure Wireless Adapter" ]; then
     bash ./systemcfg-scripts/change_wireless_interface.sh
 fi
@@ -20,6 +20,10 @@ fi
 
 if [ "$ask" == "Show the documentation" ]; then
     grip -b ~/Desktop &
+fi
+
+if [ "$ask" == "Update -- Experimental" ]; then
+  qterminal --workingfir ~ --execute "lxsudo apt update; lxsudo apt upgrade; lxsudo pip install --upgrade pip; lxsudo pip install mitmproxy; lxsudo npm install -g apk-mitm"
 fi
 
 if [ "$ask" == "Exit" ]; then
